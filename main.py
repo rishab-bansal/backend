2f15ce5c0a925a2359f12b60c36355163a2f2bd8
rishab-bansal <73712021+rishab-bansal@users.noreply.github.com>
2026-04-13 18:21:29 +0200

    Testing

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or your github pages URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def home():
-    return {"message": "Backend showing the result!"}
+    return {"message": "Backend showing"}
 
 @app.get("/test")
 def test():
