b2bd2ca02cc1558e732e17d5a24c9e7950317988
rishab-bansal <73712021+rishab-bansal@users.noreply.github.com>
2026-05-01 13:30:55 +0200

    vw_set_5

--- static/script/sb.js
+++ static/script/sb.js
@@ -3,33 +3,63 @@ import {state} from "./sb_state.js"
 const canvas = document.getElementById("canvas_sb");
 const ctx = canvas.getContext("2d");
 
-const cw = canvas.width;
-const ch = canvas.height;
+/////////// Setting canvas dimensions //////////////
+var cw = canvas.width;
+var ch = canvas.height;
+var px_per_meter = cw/10;
+var ground;
+var robot;
 
-const px_per_meter = 90;
-// 1m = 100 px
-const ground = {
-    y: 0.5*ch,
+function resizeCanvas(){
+
+    const dpr = window.devicePixelRatio || 1;
+    const width = document.documentElement.clientWidth/2;
+    const height = width/2;
+    cw = width;
+    ch = height;
+    // Set CSS size (no overflow)
+    canvas.style.width = width + "px";
+    canvas.style.height = height + "px";
+    
+    // Set internal resolution (sharp rendering)
+    canvas.width = width * dpr;
+    canvas.height = height * dpr;
+    
+    ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+    resetCanvasElements();
 }
 
-const robot = {
-    cx: 0.5*cw,
-    cs: 0.2*px_per_meter,
-    get cy(){
-        return ground.y - this.cs;
-    },
-    
-    rl: 1*px_per_meter,
-    ps: 0.1*px_per_meter,
+////////////////////////////////////////////////////
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas)
 
-    get px(){
-        return this.cx
-    },
 
-    get py(){
-        return ground.y - this.rl - this.cs
-    },
+function resetCanvasElements(){
+    px_per_meter = cw/10;
+
+    ground = {
+        y: 0.5*ch,
+    }
     
+    robot = {
+        cx: 0.5*cw,
+        cs: 0.2*px_per_meter,
+        get cy(){
+            return ground.y - this.cs;
+        },
+        
+        rl: 1*px_per_meter,
+        ps: 0.1*px_per_meter,
+        
+        get px(){
+            return this.cx
+        },
+        
+        get py(){
+            return ground.y - this.rl - this.cs
+        },
+        
+    }
 }
 
 
@@ -61,24 +91,25 @@ function drawRobot(){
 
 function drawMarkers(){
     // Simulation Time
-    ctx.font = '20px Arial';
+    let fontSize = px_per_meter*0.2;
+    ctx.font = fontSize + 'px Arial';
     ctx.fillStyle = "black";
     ctx.fillText("Simulation Time: " + Math.round(state.time*100)/100, 0.5*cw, 0.9*ch);
-    ctx.fillText(" sec", 0.66*cw, 0.9*ch);
+    ctx.fillText(" sec", 0.69*cw, 0.9*ch);
     
     // Distance Markers
     const d = px_per_meter;
     const n = cw/d;
-
+    
     ctx.beginPath();
-    ctx.arc(0.5*cw, ground.y-robot.cs, 3, 0, Math.PI*2, false);
+    ctx.arc(0.5*cw, ground.y-robot.cs, 0.03*px_per_meter, 0, Math.PI*2, false);
     ctx.fillStyle = "blue";
     ctx.fill();
     
     ctx.beginPath();
     for(let i = 1; i < n; i++){
-        ctx.arc(0.5*cw + i * d, ground.y-robot.cs, 3, 0, Math.PI*2, false);
-        ctx.arc(0.5*cw - i*d, ground.y-robot.cs, 3, 0, Math.PI*2, false);
+        ctx.arc(0.5*cw + i * d, ground.y-robot.cs, 0.03*px_per_meter, 0, Math.PI*2, false);
+        ctx.arc(0.5*cw - i*d, ground.y-robot.cs, 0.03*px_per_meter, 0, Math.PI*2, false);
     }
     ctx.fillStyle = "green";
     ctx.fill()
